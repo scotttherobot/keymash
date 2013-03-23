ad7d1f47df0f5b86e40424c55abb5e040e094db1
Scott Vanderlind <scott.vanderlind2@gmail.com>
2013-03-23 13:58:43 -0700

    New keymap, better feedback
    Also implements VT100 newline/carriage return commands for when
    interacting directly with a terminal emulator.

--- keymash.ino
+++ keymash.ino
@@ -40,68 +40,74 @@ Letter let[64];
 
 void setup(){
   /// keymap
-  let[0] = (Letter) {'.', 0, 0}; //
-  let[1] = (Letter) {'y', 0, 1}; //
-  //let[2] = (Letter) {'', 0, 2}; //BACKSPACE
-  let[3] = (Letter) {'n', 0, 3}; //
-  let[4] = (Letter) {'j', 0, 4}; //
-  let[5] = (Letter) {'h', 0, 5}; //
-  let[6] = (Letter) {'6', 0, 6}; //
-  //let[7] = (Letter) {'', 0, 7}; // CORRECT
+  let[0] = (Letter) {'k', 0, 0}; //
+  //let[1] = (Letter) {'y', 0, 1}; // END OF LINE
+  let[2] = (Letter) {'7', 0, 2};
+  //let[3] = (Letter) {'', 0, 3}; // ?????????????
+  let[4] = (Letter) {'u', 0, 4}; //
+  let[5] = (Letter) {'~', 0, 5}; // CAPS LOCK
+  //let[6] = (Letter) {'', 0, 6}; // BEEP
+  //let[7] = (Letter) {'$', 0, 7}; // SHIFT
   
-  let[8] = (Letter) {',', 1, 0}; //
-  let[9] = (Letter) {'t', 1, 1}; //
-  let[10] = (Letter) {'=', 1, 2}; //
-  let[11] = (Letter) {'b', 1, 3}; //
-  let[12] = (Letter) {'|', 1, 4}; // RETURN
-  let[13] = (Letter) {'g', 1, 5}; //
-  let[14] = (Letter) {'5', 1, 6}; //
-  //let[15] = (Letter) {'', 1, 7}; // WORDERASE
+  let[8] = (Letter) {'m', 1, 0}; //
+  let[9] = (Letter) {'r', 1, 1}; //
+  let[10] = (Letter) {'-', 1, 2}; //
+  let[11] = (Letter) {'v', 1, 3}; //
+  let[12] = (Letter) {'½', 1, 4}; //
+  let[13] = (Letter) {'f', 1, 5}; //
+  let[14] = (Letter) {'4', 1, 6}; //
+  let[15] = (Letter) {' ', 1, 7}; // SPACE
 
-  let[16] = (Letter) {'m', 2, 0}; //
-  let[17] = (Letter) {'r', 2, 1}; //
-  let[18] = (Letter) {'-', 2, 2}; //
-  let[19] = (Letter) {'v', 2, 3}; //
-  let[20] = (Letter) {'½', 2, 4}; // 
-  let[21] = (Letter) {'f', 2, 5}; //
-  let[22] = (Letter) {'4', 2, 6}; //
-  let[23] = (Letter) {' ', 2, 7}; // SPACE
+  let[16] = (Letter) {';', 2, 0}; //
+  let[17] = (Letter) {'w', 2, 1}; //
+  let[18] = (Letter) {'9', 2, 2}; //
+  let[19] = (Letter) {'x', 2, 3}; //
+  let[20] = (Letter) {'o', 2, 4}; // 
+  let[21] = (Letter) {'s', 2, 5}; //
+  let[22] = (Letter) {'2', 2, 6}; //
+  //let[23] = (Letter) {'', 2, 7}; //?????
   
-  let[24] = (Letter) {'\'', 3, 0}; //
-  let[25] = (Letter) {'e', 3, 1}; //
-  let[26] = (Letter) {'0', 3, 2}; //
-  let[27] = (Letter) {'c', 3, 3}; //
-  let[28] = (Letter) {'p', 3, 4}; // 
-  let[29] = (Letter) {'d', 3, 5}; //
-  let[30] = (Letter) {'3', 3, 6}; //
-  //let[31] = (Letter) {' ', 3, 7}; // BEEP
+  let[24] = (Letter) {'.', 3, 0}; //
+  let[25] = (Letter) {'y', 3, 1}; //
+ // let[26] = (Letter) {' ', 3, 2}; // BACKSPACE
+  let[27] = (Letter) {'n', 3, 3}; //
+  let[28] = (Letter) {'j', 3, 4}; // 
+  let[29] = (Letter) {'h', 3, 5}; //
+  let[30] = (Letter) {'6', 3, 6}; //
+  //let[31] = (Letter) {' ', 3, 7}; // ERASE A CHAR
   
-  let[32] = (Letter) {';', 4, 0}; //
-  let[33] = (Letter) {'w', 4, 1}; //
-  let[34] = (Letter) {'9', 4, 2}; //
-  let[35] = (Letter) {'x', 4, 3}; //
-  let[36] = (Letter) {'o', 4, 4}; // 
-  let[37] = (Letter) {'s', 4, 5}; //
-  let[38] = (Letter) {'2', 4, 6}; //
-  //let[39] = (Letter) {'!', 4, 7}; // no actuon
+  let[32] = (Letter) {'l', 4, 0}; //
+  let[33] = (Letter) {'q', 4, 1}; //
+  let[34] = (Letter) {'8', 4, 2}; //
+  let[35] = (Letter) {'z', 4, 3}; //
+  let[36] = (Letter) {'i', 4, 4}; // 
+  let[37] = (Letter) {'a', 4, 5}; //
+  let[38] = (Letter) {'1', 4, 6}; //
+  let[39] = (Letter) {'/', 4, 7}; //
   
-  let[40] = (Letter) {'l', 5, 0}; //
-  let[41] = (Letter) {'q', 5, 1}; //
-  let[42] = (Letter) {'8', 5, 2}; //
-  let[43] = (Letter) {'z', 5, 3}; //
-  let[44] = (Letter) {'i', 5, 4}; // 
-  let[45] = (Letter) {'a', 5, 5}; //
-  let[46] = (Letter) {'1', 5, 6}; //
-  let[47] = (Letter) {'/', 5, 7}; //
+  let[40] = (Letter) {',', 5, 0}; //
+  let[41] = (Letter) {'t', 5, 1}; //
+  let[42] = (Letter) {'=', 5, 2}; //
+  let[43] = (Letter) {'b', 5, 3}; //
+  let[44] = (Letter) {'|', 5, 4}; // LINE RETURN \n
+  let[45] = (Letter) {'g', 5, 5}; //
+  let[46] = (Letter) {'5', 5, 6}; //
+  //let[47] = (Letter) {'', 5, 7}; // WORDERASE
   
-  let[48] = (Letter) {'k', 6, 0}; //
-  //let[49] = (Letter) {'', 6, 1}; // END OF LINE
-  let[50] = (Letter) {'7', 6, 2}; //
-  //let[51] = (Letter) {'@', 6, 3}; //NO ACTUON
-  let[52] = (Letter) {'u', 6, 4}; // 
-  let[53] = (Letter) {'~', 6, 5}; //CAPS LOCK
-  //let[54] = (Letter) {'1', 6, 6}; // BEEP
-  //let[55] = (Letter) {'$', 6, 7}; // SHIFT
+  let[48] = (Letter) {'\'', 6, 0}; //
+  let[49] = (Letter) {'e', 6, 1}; //
+  let[50] = (Letter) {'0', 6, 2}; //
+  let[51] = (Letter) {'c', 6, 3}; //
+  let[52] = (Letter) {'p', 6, 4}; // 
+  let[53] = (Letter) {'d', 6, 5}; //
+  let[54] = (Letter) {'3', 6, 6}; //
+  //let[55] = (Letter) {'$', 6, 7}; // BEEP
+  
+  // ASCII support for newline, carriage return
+  let[55] = (Letter) {10, 5, 4};
+  let[56] = (Letter) {13, 5, 4};
+  // ASCII/VT100 support for Delete
+  let[57] = (Letter) {127, 3, 7};
   
  // Turn the LED on to show that setup is happening
  pinMode(LED, OUTPUT);
@@ -131,7 +137,7 @@ void setup(){
 }
 
 void loop(){
-//  mash();
+//mash();
 char c;
 while(!Serial.available()){
 }
@@ -227,7 +233,7 @@ void printc(char c){
         for(i=0; i < 64; i++){
          if(let[i].c == c){
           touch(let[i].row, let[i].col);
-          serial.print('G');
+          Serial.print(c);
           break;
          } 
         }
