947eb5a5957cfcd2cbd16a2832c9c05cefe05a35
Scott Vanderlind <scott.vanderlind2@gmail.com>
2013-03-25 21:39:21 -0700

    Completely Rewrite Tweetmash
    Now it actually formats the tweets before they are printed. The file is
    parsed into a list of Tweet objects, each having an author and a
    message. Then, one-by-one, the list of tweets is printed, making sure
    that each word in its entirety will fit on the line (real, bona-fide
    word wrap). It also arranges the page into two columns: twitter handle
    on the left, and tweets on the right (all justified to 20 characters
    from the left margin).
    
    This isn't very DRY programatically, but it works.

--- tweetmash.py
+++ tweetmash.py
@@ -1,37 +1,74 @@
-import time, sys, os, serial
+import time, sys, re, os, serial
+
+class Tweet:
+	author = ''
+	message = ''
+	def __init__(self, a, m):
+		self.author = a
+		self.message = m
 
 def main(argv):
-   f = open(argv[0])
-   ser = serial.Serial(
-         port=argv[1],
-         baudrate=1200
-   )
-   if ser.isOpen():
-      for line in f:
-         count = 0
-         for char in line:
-            # The typewriter doesn't have a < character,
-            # So we'll replace it with the @ since it only really ever shows
-            # up before a twitter handle.
-            # Word wrapping: Count characters
-            # TODO: Fix word wrap so it straightens out the provided sentence
-            # before wrapping in order to optimize paper usage.
-            count += 1
-            if count is 60:
-               ser.write('|')
-               count = 0
-               time.sleep(1.5)
-               for a in range(23):
-                  ser.write(' ')
-                  count += 1
-                  time.sleep(.15)
-            if char is '<':
-               char = '@'
-            ser.write(char)
-            time.sleep(.15)
-         time.sleep(1.5)
-      #ser.write('|')
-      ser.close()
+#def printTweets(cols):
+	cols = 65
+	left = 20
+	tweets = []
+	f = open(argv[0]).read().replace('\n', ' ')
+	raw = f.split()
+	# Let's parse all the incoming tweets and put them in a list
+	for tok in raw:
+		if tok.startswith('<') and tok.endswith('>'):
+			tok = '@' + tok[1:-1]
+			tweets.append(Tweet(tok, ''))
+		else:
+			tweets[-1].message = " ".join([tweets[-1].message,tok])
+
+	# Now to send them to the typewriter, prettified
+	ser = serial.Serial(
+		port=argv[1],
+		baudrate=1200
+	)	
+	if ser.isOpen():
+		for t in tweets:
+			count = 0
+			# Print the author's name
+			for c in t.author:
+				count += 1
+				ser.write(c)
+				time.sleep(.15)
+			# Some whitespace to make a divider
+			while (count < left):
+				ser.write(' ')
+				count +=1
+				time.sleep(.15)
+			
+			# Now to print the actual tweet
+			words = t.message.split()
+			for word in words:
+				proj = len(word) + count	
+				if count < cols:
+					for c in word:
+						ser.write(c)
+						time.sleep(.15)
+						count += 1
+				else:
+					ser.write('|')
+					time.sleep(1.5)
+					count = 0
+					while (count < left):
+						ser.write(' ')
+						count += 1
+						time.sleep(.15)
+					for c in word:
+						ser.write(c)
+						count += 1
+						time.sleep(.15)
+				ser.write(' ')
+				count += 1
+				time.sleep(.15)
+			# After that tweet's done, go to a new line for the next
+			ser.write('|')
+			time.sleep(1.5)
+	ser.close()
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+	main(sys.argv[1:])
